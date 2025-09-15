0fba26a5c5e6d4808bf53de762c5a8dd613d0104
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-09-15 20:03:36 +0600

    update student model

--- models/student.go
+++ models/student.go
@@ -11,4 +11,9 @@ type Student struct {
 	Subject       string             `bson:"subject" json:"subject"`
 	PaymentStatus bool               `bson:"payment_status" json:"payment_status"`
     PaymentAmount float64            `bson:"payment_amount" json:"payment_amount"`
+    PaidMonths    []string           `bson:"paid_months" json:"paid_months"`
+    DueMonths     []string           `bson:"due_months" json:"due_months"`
+    // satureday , monday, wednesday - smw 
+    // sunday , tuesday, thursday - stt
+    StudayDays    []string           `bson:"study_days" json:"study_days"`
 }
